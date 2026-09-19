35e3265e6d7219b58da262406f60a1bbb9f21d5b
maranowacki <ma_nowac@live.concordia.ca>
2026-09-19 15:01:28 -0400

    fixed kitty again

--- Topics/Prototyping/Representational Prototype/js/script.js
+++ Topics/Prototyping/Representational Prototype/js/script.js
@@ -7,7 +7,7 @@ function draw() {
   background(255);
   fill(0);
 
-  // Tail 1
+  // Tail
   taperedStroke(
     [[300, 440], [268, 395], [250, 330], [252, 250], [275, 195], [315, 165],
      [350, 175], [378, 215], [388, 275], [378, 330], [350, 358], [320, 355],
@@ -35,7 +35,7 @@ function draw() {
   vertex(416, 338); 
   vertex(430, 356);
   vertex(446, 358);
-  vertex(458, 344); 
+  vertex(458, 344);
   vertex(462, 358);
   vertex(460, 384);
   vertex(450, 406);
@@ -43,7 +43,7 @@ function draw() {
   vertex(432, 470);
   endShape(CLOSE);
 
-  // Legs
+  // Legs 1
   stroke(0);
   strokeWeight(13);
   strokeCap(ROUND);
@@ -52,6 +52,11 @@ function draw() {
   leg([[324, 482], [322, 530], [326, 566], [333, 580], [342, 584]]);
   leg([[395, 482], [393, 530], [395, 566], [400, 576], [406, 578]]);
   leg([[420, 482], [418, 530], [420, 566], [425, 576], [432, 578]]);
+}
+
+function crPoint(a, b, c, d, t) {
+  const t2 = t * t, t3 = t2 * t;
+  return 0.5 * ((2 * b) + (-a + c) * t + (2 * a - 5 * b + 4 * c - d) * t2 + (-a + 3 * b - 3 * c + d) * t3);
 }
 
 function sampleCurve(pts, step = 1.5) {
@@ -66,14 +71,15 @@ function sampleCurve(pts, step = 1.5) {
     for (let j = 0; j < k; j++) {
       const t = j / k;
       out.push([
-        curvePoint(p0[0], p1[0], p2[0], p3[0], t),
-        curvePoint(p0[1], p1[1], p2[1], p3[1], t)
+        crPoint(p0[0], p1[0], p2[0], p3[0], t),
+        crPoint(p0[1], p1[1], p2[1], p3[1], t)
       ]);
     }
   }
   out.push(pts[n - 1]);
   return out;
 }
+
 
 function taperedStroke(pts, widths) {
   const s = sampleCurve(pts);
@@ -87,10 +93,11 @@ function taperedStroke(pts, widths) {
   }
 }
 
-
+// Leg 2
 function leg(pts) {
   const s = sampleCurve(pts);
   beginShape();
   for (const [x, y] of s) vertex(x, y);
   endShape();
 }
+
